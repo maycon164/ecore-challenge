1af915fb1c19c73a7f39b7c39f0d45b264235a8a
maycon164 <mayconfelipe164@gmail.com>
2021-10-13 08:33:24 -0300

    utils

---
+++ utils/validator.js
@@ -0,0 +1,26 @@
+module.exports = function (name, age) {
+    
+    let regex = /[0-9]/
+
+    if (!name) {
+        throw new Error("name is empty");
+    }
+
+    if (!isNaN(name) | regex.test(name)) {
+        throw new Error("name can't be a number");
+    }
+
+    if (age < 0) {
+        throw new Error("age must be greater than 0");
+    }
+
+    if (!age) {
+        throw new Error("age is empty");
+    }
+
+    if (isNaN(age)) {
+        throw new Error("age must be a number");
+    }
+
+    return true;
+}
